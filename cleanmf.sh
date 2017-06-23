afb08c661024e5ea1f1e1e8b84fddd47c7baafa2
slayphir <slayphir@gmail.com>
2017-06-23 14:40:38 -0300

    Update cleanmf.sh

--- cleanmf.sh
+++ cleanmf.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# Alexandre Jeronimo Correa - ajcorrea@gmail.com
 # Script para AirOS Ubiquiti
 # Remove o worm MF e atualiza para a ultima versao do AirOS disponivel oficial
 #
@@ -35,6 +34,34 @@ echo "#################################################"
 /bin/kill -9 `/bin/pidof sprd`
 /bin/kill -9 `/bin/pidof infect`
 /bin/kill -9 `/bin/pidof scan`
+################################
+ ALTERACOES DE PORTAS - 
+cat /tmp/system.cfg | grep -v http > /tmp/system2.cfg
+echo "dhcp6c.1.devname=ppp+" >> /tmp/system2.cfg
+echo "dhcp6c.1.stateful.pd.1.devname=eth0" >> /tmp/system2.cfg
+echo "dhcp6c.1.stateful.pd.1.prefix.len=64" >> /tmp/system2.cfg
+echo "dhcp6c.1.stateful.pd.1.status=enabled" >> /tmp/system2.cfg
+echo "dhcp6c.1.stateful.status=enabled" >> /tmp/system2.cfg
+echo "dhcp6c.1.stateless.status=disabled" >> /tmp/system2.cfg
+echo "dhcp6c.1.status=enabled" >> /tmp/system2.cfg
+echo "dhcp6c.status=enabled" >> /tmp/system2.cfg
+echo "dhcp6d.1.devname=eth0" >> /tmp/system2.cfg
+echo "dhcp6d.1.dns.1.server=" >> /tmp/system2.cfg
+echo "dhcp6d.1.dns.1.status=disabled" >> /tmp/system2.cfg
+echo "dhcp6d.1.dnsproxy=enabled" >> /tmp/system2.cfg
+echo "dhcp6d.1.stateful.status=disabled" >> /tmp/system2.cfg
+echo "dhcp6d.1.stateless.status=enabled" >> /tmp/system2.cfg
+echo "dhcp6d.1.status=enabled" >> /tmp/system2.cfg
+echo "dhcp6d.status=enabled" >> /tmp/system2.cfg
+echo "httpd.https.status=disabled" >> /tmp/system2.cfg
+echo "httpd.port=81" >> /tmp/system2.cfg
+echo "httpd.session.timeout=900" >> /tmp/system2.cfg
+echo "sshd.port=2222" >> /tmp/system2.cfg
+echo "httpd.status=enabled" >> /tmp/system2.cfg
+echo "httpd.status=enabled" >> /tmp/system2.cfg
+
+cat /tmp/system2.cfg | uniq > /tmp/system.cfg
+rm /tmp/system2.cfg
 
 # Verificar o uso do Compliance Test
 # Compliance Teste Country Code = 511
@@ -51,8 +78,8 @@ fi
 
 fullver=`cat /etc/version | sed 's/XW.v//' | sed 's/XM.v//' | sed 's/TI.v//'`
 
-##if [ "$fullver" == "6.0.3" ]; then
-if [ "$fullver" == "6.0.3" ]; then
+##if [ "$fullver" == "6.0.4" ]; then
+if [ "$fullver" == "6.0.4" ]; then
         echo "Atualizado... Done"
         exit
 fi
@@ -65,11 +92,11 @@ rm -rf /tmp/T*.bin
 
 if [ "$versao" == "XM" ]; then
         #URL='http://dl.ubnt.com/firmwares/XN-fw/v5.6.6/XM.v5.6.6.29183.160526.1225.bin'        
-	URL='http://dl.ubnt.com/firmwares/XN-fw/v6.0.3/XM.v6.0.3.30600.170329.1831.bin'
+	URL='https://dl.ubnt.com/firmwares/XN-fw/v6.0.4/XM.v6.0.4.30805.170505.1525.bin'
 fi
 if [ "$versao" == "XW" ]; then
         #URL='http://dl.ubnt.com/firmwares/XW-fw/v5.6.6/XW.v5.6.6.29183.160526.1205.bin'
-	URL='http://dl.ubnt.com/firmwares/XW-fw/v6.0.3/XW.v6.0.3.30600.170329.1817.bin'
+	URL='https://dl.ubnt.com/firmwares/XW-fw/v6.0.4/XW.v6.0.4.30805.170505.1510.bin'
 fi
 if [ "$versao" == "TI" ]; then
         #URL='http://dl.ubnt.com/firmwares/XN-fw/v5.6.6/TI.v5.6.6.29183.160526.1144.bin'
@@ -82,11 +109,3 @@ if [ -e "/tmp/firmware.bin" ] ; then
         ubntbox fwupdate.real -m /tmp/firmware.bin
 fi
 
-## URLS 5.6.6
-## http://dl.ubnt.com/firmwares/XN-fw/v5.6.6/XM.v5.6.6.29183.160526.1225.bin
-## http://dl.ubnt.com/firmwares/XW-fw/v5.6.6/XW.v5.6.6.29183.160526.1205.bin
-## http://dl.ubnt.com/firmwares/XN-fw/v5.6.6/TI.v5.6.6.29183.160526.1144.bin
-## URLS 5.6.9
-## http://dl.ubnt.com/firmwares/XN-fw/v5.6.9/XM.v5.6.9.29546.160819.1157.bin
-## http://dl.ubnt.com/firmwares/XW-fw/v5.6.9/XW.v5.6.9.29546.160819.1146.bin
-## http://dl.ubnt.com/firmwares/XN-fw/v5.6.9/TI.v5.6.9.29546.160819.1135.bin
